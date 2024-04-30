e43ea652626e41ce3eb18a7923a43c22f6c8f5fc
Tu Nombre <a.macianruiz@edu.gva.es>
2024-04-30 12:39:26 +0200

    v1

---
+++ app-files/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-aula-ateca",
+      "name": "Aula Ateca",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.2630964612602291,
+        "pitch": 0.6150670855816394,
+        "fov": 1.279946808713648
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.882783436557137,
+          "pitch": 0.2686527352748236,
+          "rotation": 0,
+          "target": "1-aula-emprem"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2345628256565515,
+          "pitch": 0.09093703521756602,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Pantalla táctil</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2885288994603243,
+          "pitch": 1.5482801448153474,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Cámara 360º</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.4546190642031434,
+          "pitch": 0.2503414501712804,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Ordenadores de Alto Rendimiento</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.9505964433715484,
+          "pitch": -0.5774025837816339,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Proyector</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-aula-emprem",
+      "name": "Aula Emprem",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.21097920171309603,
+        "pitch": 0.32328337533214757,
+        "fov": 1.279946808713648
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12562149876888284,
+          "pitch": 0.2953004632353782,
+          "rotation": 0,
+          "target": "0-aula-ateca"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3739713210669224,
+          "pitch": 0.1311495204261508,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Pantalla táctil</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.3895698577708409,
+          "pitch": 1.5458145297629553,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Cámara 360º</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.6689599094691374,
+          "pitch": -0.4299707915060935,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Proyector</font></font>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
